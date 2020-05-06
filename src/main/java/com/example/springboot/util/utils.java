35845a582dbbb61b845f55a8bef891f0786b3627
SamYuan1990 <yy19902439@126.com>
2020-05-06 21:59:38 +0800

    sample refactor

--- src/main/java/com/example/springboot/util/utils.java
+++ src/main/java/com/example/springboot/util/utils.java
@@ -13,8 +13,8 @@ import static java.lang.String.format;
 
 public class utils {
 
-    public static String config_network_path = "./src/main/resources/Networkconfig.json";
-    public static String config_user_path = ".././fabric-samples/first-network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore";
+    public static String networkConfig = "./src/main/resources/Networkconfig.json";
+    public static String configUserPath = "./src/main/resources/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore";
     public static ArrayList<ByteString> x509Header = new ArrayList<ByteString>();
 
     public static File findFileSk(File directory) {
@@ -40,5 +40,47 @@ public class utils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static String QueryWithOutPool(){
+        String payload = "";
+        try {
+            User appuser = null;
+            File sampleStoreFile = new File(System.getProperty("user.home") + "/test.properties");
+            if (sampleStoreFile.exists()) { //For testing start fresh
+                sampleStoreFile.delete();
+            }
+            final SampleStore sampleStore = new SampleStore(sampleStoreFile);
+            appuser = sampleStore.getMember("peer1", "Org1", "Org1MSP",
+                    new File(String.valueOf(findFileSk(Paths.get(configUserPath).toFile()))),
+                    new File("./src/main/resources/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem"));
+            Channel myChannel;
+            HFClient hfclient = HFClient.createNewInstance();
+            CryptoSuite cryptoSuite = CryptoSuite.Factory.getCryptoSuite();
+            hfclient.setCryptoSuite(cryptoSuite);
+            NetworkConfig networkConfig = NetworkConfig.fromJsonFile(new File(config_network_path));
+            hfclient.setUserContext(appUser);
+            hfclient.loadChannelFromConfig(channel, networkConfig);
+            myChannel = hfclient.getChannel(channel);
+            myChannel.initialize();
+            ChaincodeID chaincodeID = ChaincodeID.newBuilder().setName(chainCodeName)
+                    .setVersion("1.0")
+                    .build();
+            HFClient hfclient = HFClient.createNewInstance();
+            QueryByChaincodeRequest transactionProposalRequest = hfclient.newQueryProposalRequest();
+            transactionProposalRequest.setChaincodeID(chaincodeID);
+            transactionProposalRequest.setFcn(fcn);
+            transactionProposalRequest.setArgs(arguments);
+            transactionProposalRequest.setUserContext(getUser());
+            Collection<ProposalResponse> queryPropResp = myChannel.queryByChaincode(transactionProposalRequest);
+            for (ProposalResponse response:queryPropResp) {
+                if (response.getStatus() == ChaincodeResponse.Status.SUCCESS) {
+                    payload = response.getProposalResponse().getResponse().getPayload().toStringUtf8();
+                }
+            }
+        } catch (Exception e) {
+            System.out.printf(e.toString());
+        }
+        return payload;
     }
 }
